c4252021c6786eedd9988728f7e17aee913a099e
yagiken1021 <yagiken1228@gmail.com>
2021-05-13 21:19:23 +0900

    2021.05.13

--- regist.php
+++ regist.php
@@ -1,12 +1,3 @@
-<?php
-    mb_internal_encoding("utf8");
-    $pdo=new PDO("mysql:dbname=lessen02;host=localhost;","root","");
-    $stmt=$pdo->query("select * from account");
-    
-    
-    }
-    ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -59,6 +50,78 @@
         <input type="number" size="10" name="postal_code" required autofocus>
         <br>
         <p id="notice-input-postal_code" style="display:none; color:red;">郵便番号が未入力です</p>
+        <label>住所（都道府県）</label>
+        <br>
+        <select name="prefecture">
+            <option value="" selected>都道府県</option>
+            <option value="北海道">北海道</option>
+            <option value="青森県">青森県</option>
+            <option value="岩手県">岩手県</option>
+            <option value="宮城県">宮城県</option>
+            <option value="秋田県">秋田県</option>
+            <option value="山形県">山形県</option>
+            <option value="福島県">福島県</option>
+            <option value="茨城県">茨城県</option>
+            <option value="栃木県">栃木県</option>
+            <option value="群馬県">群馬県</option>
+            <option value="埼玉県">埼玉県</option>
+            <option value="千葉県">千葉県</option>
+            <option value="東京都">東京都</option>
+            <option value="神奈川県">神奈川県</option>
+            <option value="新潟県">新潟県</option>
+            <option value="富山県">富山県</option>
+            <option value="石川県">石川県</option>
+            <option value="福井県">福井県</option>
+            <option value="山梨県">山梨県</option>
+            <option value="長野県">長野県</option>
+            <option value="岐阜県">岐阜県</option>
+            <option value="静岡県">静岡県</option>
+            <option value="愛知県">愛知県</option>
+            <option value="三重県">三重県</option>
+            <option value="滋賀県">滋賀県</option>
+            <option value="京都府">京都府</option>
+            <option value="大阪府">大阪府</option>
+            <option value="兵庫県">兵庫県</option>
+            <option value="奈良県">奈良県</option>
+            <option value="和歌山県">和歌山県</option>
+            <option value="鳥取県">鳥取県</option>
+            <option value="島根県">島根県</option>
+            <option value="岡山県">岡山県</option>
+            <option value="広島県">広島県</option>
+            <option value="山口県">山口県</option>
+            <option value="徳島県">徳島県</option>
+            <option value="香川県">香川県</option>
+            <option value="愛媛県">愛媛県</option>
+            <option value="高知県">高知県</option>
+            <option value="福岡県">福岡県</option>
+            <option value="佐賀県">佐賀県</option>
+            <option value="長崎県">長崎県</option>
+            <option value="熊本県">熊本県</option>
+            <option value="大分県">大分県</option>
+            <option value="宮崎県">宮崎県</option>
+            <option value="鹿児島県">鹿児島県</option>
+            <option value="沖縄県">沖縄県</option>
+        </select>
+        <br>
+        <p id="notice-select-prefecture" style="display:none; color:red;">住所（都道府県）が未入力です</p>
+        <label>住所（市区町村）</label>
+        <br>
+        <input type="text" size="10" name="address1" pattern="[^\x20-\x7E]*" required autofocus>
+        <br>
+        <p id="notice-input-address1" style="display:none; color:red;">住所（市区町村）が未入力です</p>
+        <label>住所（番地）</label>
+        <br>
+        <input type="text" size="10" name="address2" pattern="[^\x20-\x7E]*" required autofocus>
+        <br>
+        <p id="notice-input-address2" style="display:none; color:red;">住所（番地）が未入力です</p>
+        <label>管理者権限</label>
+        <br>
+        <select name="authority">
+            <option value="一般" selected>一般</option>
+            <option value="管理者">管理者</option>
+        </select>
+        <p id="notice-select-autority" style="display:none; coler:red;">管理者権限が未入力です</p>
+        <input type="submit" value="登録する">
     </form>
 </body>
 
---
+++ regist_confirm.php
@@ -0,0 +1,28 @@
+<!DOCTYPE htmel>
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+<title>アカウント登録確認画面</title>
+
+<body>
+    <?php
+    $family_name=$_POST['family_name'];
+    $last_name=$_POST['last_name'];
+    $family_name_kana=$_POST['family_name_kana'];
+    $last_name_kana=$_POST['last_name_kana'];
+    $mail=$_POST['mail'];
+    $password=$_POST['password'];
+    $gender=$_POST['gender'];
+    $postal_code=$_POST['postal_code'];
+    $prefecture=$_POST['prefecture'];
+    $address1=$_POST['address1'];
+    $address2=$_POST['address2'];
+    authoriry=$_POST['authority'];
+    
+    
+    ?>
+</body>
+
+</html>
