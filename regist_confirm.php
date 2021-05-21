54ccae8d276bd6eaf2ca150663a53a1d772a6d63
yagiken1021 <yagiken1228@gmail.com>
2021-05-21 21:25:43 +0900

    2021.05.21

--- regist_confirm.php
+++ regist_confirm.php
@@ -23,6 +23,33 @@
     
     
     ?>
+    <h1>アカウント登録画面</h1>
+    <div class="confirm">
+        <h2>名前（姓）</h2>
+        <p><?php echo $_POST['family_name']; ?></p>
+        <h2>名前（名）</h2>
+        <p><?php echo $_POST['last_name']; ?></p>
+        <h2>カナ（姓）</h2>
+        <p><?php echo $_POST['family_name_kana']; ?></p>
+        <h2>カナ（名）</h2>
+        <p><?php echo $_POST['last_name_kana']; ?></p>
+        <h2>メールアドレス</h2>
+        <p><?php echo $_POST['mail']; ?></p>
+        <h2>パスワード</h2>
+        <p>●●●●●●●●</p>
+        <h2>性別</h2>
+        <p><?php echo $_POST['gender']; ?></p>
+        <h2>郵便番号</h2>
+        <p><?php echo $_POST['postal_code']; ?></p>
+        <h2>住所（都道府県）</h2>
+        <p><?php echo $_POST['prefecture']; ?></p>
+        <h2>住所（市区町村）</h2>
+        <p><?php echo $_POST['address_1']; ?></p>
+        <h2>住所（番地）</h2>
+        <p><?php echo $_POST['address_2']; ?></p>
+        <h2>アカウント権限</h2>
+        <p><?php echo $_POST['authority']; ?></p>
+    </div>
 </body>
 
 </html>
